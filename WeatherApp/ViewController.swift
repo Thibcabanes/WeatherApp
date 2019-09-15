c81cca5307d92ad233b8a91276f3a603251b806c
Gabriel Cabanes <gabriel.cabanes0@gmail.com>
2019-09-15 06:34:08 +0200

    Handling Pressure and Wind

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var max: UILabel!
     @IBOutlet weak var city: UILabel!
     @IBOutlet weak var temp: UILabel!
+    @IBOutlet weak var windSpeed: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var windDir: UILabel!
     
     var current = 0
     var hourlyForecast = [Hourly]()
@@ -36,12 +39,36 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    func bearingToDistance(bearing: Int) -> String {
+        if bearing > 330 || bearing < 30 {
+            return "Nord"
+        } else if bearing > 30 && bearing < 75 {
+            return "Nord Est"
+        } else if bearing > 75 && bearing < 120 {
+            return "Est"
+        } else if bearing > 120 && bearing < 150 {
+            return "Sud Est"
+        } else if bearing > 150 && bearing < 210 {
+            return "Sud"
+        } else if bearing > 210 && bearing < 240 {
+            return "Sud Ouest"
+        } else if bearing > 240 && bearing < 300 {
+            return "Ouest"
+        } else if bearing > 300 && bearing < 330 {
+            return "Nord Ouest"
+        }
+        return "NUL/20"
+    }
 
     func updateFields() {
         min.text = "❄️ \(Int(self.dailyForecast[0].temperatureLow)) °F"
         max.text = "🔥 \(Int(self.dailyForecast[0].temperatureHigh)) °F"
         icon.image = UIImage(named: self.hourlyForecast[self.current].icon)
         temp.text = "\(Int(self.hourlyForecast[self.current].temperature)) °F"
+        windSpeed.text = "\(Int(self.hourlyForecast[self.current].windSpeed))km/h"
+        windDir.text = "Vent \(bearingToDistance(bearing: Int(self.hourlyForecast[self.current].pressure)))"
+        pressure.text = "\(Int(self.hourlyForecast[self.current].pressure))hPa"
     }
 }
 
