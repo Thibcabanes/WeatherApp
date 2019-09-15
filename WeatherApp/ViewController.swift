38ed27bf9ce8f296076d1f84186c8b7e46baf29b
Gabriel Cabanes <gabriel.cabanes0@gmail.com>
2019-09-15 05:40:04 +0200

    manipuling data with storyboard

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var min: UILabel!
+    @IBOutlet weak var max: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var temp: UILabel!
+    
+    var current = 0
+    var hourlyForecast = [Hourly]()
+    var dailyForecast = [Daily]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("App Started")
+        updateLocation(location: "37.8267,-122.4233")
+    }
+    
+    func updateLocation(location: String) {
         API.forecast(withLocation: "37.8267,-122.4233") { (hourly: [Hourly], daily: [Daily]) in
-            print("\(hourly.count)\n\n")
-            print("\(daily.count)\n\n")
+            self.hourlyForecast = hourly
+            self.dailyForecast = daily
+            DispatchQueue.main.async {
+                self.updateFields()
+            }
         }
+    }
+
+    func updateFields() {
+        min.text = "❄️ \(Int(self.dailyForecast[0].temperatureLow)) °F"
+        max.text = "🔥 \(Int(self.dailyForecast[0].temperatureHigh)) °F"
+        icon.image = UIImage(named: self.hourlyForecast[self.current].icon)
+        temp.text = "\(Int(self.hourlyForecast[self.current].temperature)) °F"
     }
 }
 
